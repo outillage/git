7fb9363fe2d460239d7971fac79728ca837de513
Simon Prochazka <simon@fallion.net>
2019-10-21 11:46:14 +0200

    feat(getTags): returns all tags in the repo
    - reworks PreviousTag to use getTags
    - moves sorting logic into getTags

---
+++ pkg/get_tags.go
@@ -0,0 +1,57 @@
+package history
+
+import (
+	"log"
+	"sort"
+
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+)
+
+func (g *Git) getTags() ([]Tag, error) {
+	tagrefs, err := g.repo.Tags()
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer tagrefs.Close()
+
+	var tags []Tag
+
+	err = tagrefs.ForEach(func(t *plumbing.Reference) error {
+		commitDate, err := g.commitDate(t.Hash())
+
+		if err != nil {
+			return err
+		}
+
+		tags = append(tags, Tag{Name: t.Name().String(), Date: commitDate, Hash: t.Hash()})
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Tags are alphabetically ordered. We need to sort them by date.
+	sortedTags := sortTags(g.repo, tags)
+
+	if g.Debug {
+		log.Println("Sorted tag output: ")
+		for _, taginstance := range sortedTags {
+			log.Printf("hash: %v time: %v", taginstance.Hash, taginstance.Date.UTC())
+		}
+	}
+
+	return sortedTags, nil
+}
+
+// sortTags sorts the tags according to when their parent commit happened.
+func sortTags(repo *git.Repository, tags []Tag) []Tag {
+	sort.Slice(tags, func(i, j int) bool {
+		return tags[i].Date.After(tags[j].Date)
+	})
+
+	return tags
+}
---
+++ pkg/get_tags_test.go
@@ -0,0 +1,24 @@
+package history
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+func TestGetTags(t *testing.T) {
+	repo, err := git.PlainOpen("../testdata/git_tags")
+	testGit := &Git{repo: repo, Debug: false}
+
+	assert.NoError(t, err)
+
+	tags, err := testGit.getTags()
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(tags))
+
+	assert.Equal(t, "refs/tags/v0.0.2", tags[0].Name)
+	assert.Equal(t, "refs/tags/v0.0.1", tags[1].Name)
+
+}
