ae8b9fcb2efd4b7deb119c632e99d08a6c25836a
Simon Prochazka <simon@fallion.net>
2019-10-21 12:00:49 +0200

    feat(CurrentTag): add function to get tag on current commit

---
+++ pkg/current_tag_test.go
@@ -0,0 +1,31 @@
+package history
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+func TestCurrentTagHappy(t *testing.T) {
+	repo, err := git.PlainOpen("../testdata/git_tags")
+	testGit := &Git{repo: repo, Debug: true}
+
+	assert.NoError(t, err)
+
+	tag, err := testGit.CurrentTag()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "refs/tags/v0.0.2", tag.Name)
+}
+
+func TestCurrentTagUnhappy(t *testing.T) {
+	repo := setupRepo()
+	createTestHistory(repo)
+
+	testGit := &Git{repo: repo}
+
+	_, err := testGit.CurrentTag()
+
+	assert.Equal(t, ErrCommitNotOnTag, err)
+}
