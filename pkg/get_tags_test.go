de4084fb6e41003a57c3abebc16dd74f7601b469
Simon Prochazka <simon@fallion.net>
2019-10-25 13:39:13 +0200

    fix(getTags): annotated tags were not detected correctly by getTags
    - adds test case for annotated tags
    - adds logic to get annotated tags as well

--- pkg/get_tags_test.go
+++ pkg/get_tags_test.go
@@ -22,3 +22,19 @@ func TestGetTags(t *testing.T) {
 	assert.Equal(t, "refs/tags/v0.0.1", tags[1].Name)
 
 }
+
+func TestAnnotatedGetTags(t *testing.T) {
+	repo, err := git.PlainOpen("../testdata/annotated_git_tags_mix")
+	testGit := &Git{repo: repo, Debug: false}
+
+	assert.NoError(t, err)
+
+	tags, err := testGit.getTags()
+
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(tags))
+
+	assert.Equal(t, "v0.0.3", tags[0].Name)
+	assert.Equal(t, "refs/tags/v0.0.2", tags[1].Name)
+
+}
--- testdata/setup_test_repos.sh
+++ testdata/setup_test_repos.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-rm -rf testdata/commits_on_branch
-rm -rf testdata/git_tags
+cd testdata
 
-if [ ! -d './testdata/commits_on_branch' ]; then
-  cd testdata
+echo 'testdata/commits_on_branch_test/ directory does not exist at the root; creating...'
+rm -rf commits_on_branch
+git clone commits_on_branch.bundle
+echo 'done'
 
-  echo 'testdata/commits_on_branch_test/ directory does not exist at the root; creating...'
-  git clone commits_on_branch.bundle
-  echo 'done'
+echo 'testdata/git_tags/ directory does not exist at the root; creating...'
+rm -rf git_tags
+git clone git_tags.bundle
+echo 'done'
 
-  echo 'testdata/git_tags/ directory does not exist at the root; creating...'
-  git clone git_tags.bundle
-  echo 'done'
-  
-else
-  echo 'testdata/commits_on_branch_test directory already exists; aborting.'
-  exit -1
-fi
+echo 'testdata/annotated_git_tags_mix/ directory does not exist at the root; creating...'
+rm -rf annotated_git_tags_mix
+git clone annotated_git_tags_mix.bundle
+echo 'done'
