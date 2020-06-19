bc470a8a562aee0a20e138891a7dbf89e8cbeb4f
Fallion <simon@fallion.net>
2020-06-19 21:43:37 +0200

    feat: add CommitsOnBranchSimple
    Uses the bare refs instead of full objects

---
+++ commits_on_branch_simple.go
@@ -0,0 +1,42 @@
+package git
+
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+type SimpleCommit struct {
+	Hash    [20]byte
+	Message string
+}
+
+// CommitsOnBranchSimple iterates through all references and returns simpleCommits on given branch. \n
+// Important to note is that this will provide all commits from anything the branch is connected to.
+func (g *Git) CommitsOnBranchSimple(
+	branchCommit plumbing.Hash,
+) ([]SimpleCommit, error) {
+	var branchCommits []SimpleCommit
+
+	branchIter, err := g.repo.Log(&git.LogOptions{
+		From: branchCommit,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	branchIterErr := branchIter.ForEach(func(commit *object.Commit) error {
+		branchCommits = append(branchCommits, SimpleCommit{
+			Hash:    commit.Hash,
+			Message: commit.Message,
+		})
+		return nil
+	})
+
+	if branchIterErr != nil {
+		g.DebugLogger.Printf("Stopped getting commits on branch: %v", branchIterErr)
+	}
+
+	return branchCommits, nil
+}
